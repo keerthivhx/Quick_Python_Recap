3d22804c248173d79cca78bb3d80b9a53e30023d
keerthivhx <keerthivkeerthiv182@gmail.com>
2026-04-16 11:51:07 +0530

    updated the index concept

---
+++ index.py
@@ -0,0 +1,14 @@
+Name="Keerthi r you going??"
+print(len(Name))
+keer=Name[0:3]
+print(keer)
+eerthi=Name[1:]
+print(eerthi)
+keerthi=Name[:].lower()
+print(keerthi)
+keerthi1=Name[0:-1]
+print(keerthi1)
+keerthi2=Name[-1:-1]
+print(keerthi2)
+keerthi3=Name[-1]
+print(keerthi3)
