410a954144a8248076eef0ae596fab32cd96835e
keerthivhx <keerthivkeerthiv182@gmail.com>
2026-04-18 18:04:44 +0530

    updated files

---
+++ Variable_len.py
@@ -0,0 +1,4 @@
+#*arg
+def sum1(*a):
+    return sum(a)
+print(sum1(6,7,9,6,9))
---
+++ function.py
@@ -0,0 +1,15 @@
+# def any_fun(name):#arguments
+#     print(f"Hello,{name}")
+# any_fun("keerthi")#positional argument
+# any_fun("keerthi1")
+# any_fun(name="keerthi2")#keyword argument
+# any_fun(name="keerthi3")#keyword argument
+# let me write the multiple of 3
+def multiple(i,h="ii"):
+    for j in range(1,11,1):
+        print(f"{i}*{j}={i*j}")
+        print(h)
+multiple(3)
+# multiple(5)
+# multiple(7)
+multiple(3)
